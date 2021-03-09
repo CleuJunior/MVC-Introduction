fc38af2c99e669efdde74a652839def52572c020
CleuJunior <cleu.junior@gmail.com>
2021-03-09 01:04:31 -0300

    Criando rotas

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "MVC is Awesome";
+require '../Core/Router.php';
+
+// echo 'Requested URL = ' . $_SERVER['QUERY_STRING'];
+
+$router = new Router();
+
+// echo get_class($router);
+
+// Add the routes
+
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('post', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
